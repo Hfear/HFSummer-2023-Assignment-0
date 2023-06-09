5ea7cb6a9df42fc1a216c90e64349277ba037953
Hfear <hannahbfearns@gmail.com>
2023-06-09 16:47:39 -0400

    tested and finished pairsum 18

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,28 @@
 function pairSum(nums, target) {
-  // Insert code here;
+    
+  let compliment; 
+
+  if(nums.length <= 1)
+{
+  throw new Error('array length too small');
+}
+  for (let i of nums)
+  {
+
+      compliment = target - i; 
+      //let sum = parseInt(i) + parseInt(compliment);
+
+      //console.log(i + " + " + compliment + " = " +  sum);
+
+      if(nums.includes(compliment))
+      {
+           return (true);
+           
+      }
+
+  }
+
+  return (false);
 }
 
 // Do not edit this line;
