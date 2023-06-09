bef8aff45843131492c4b6cbc79620e452fe1918
Hfear <hannahbfearns@gmail.com>
2023-06-09 11:04:14 -0400

    test n upload 11

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,22 @@
 function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
+
+  //setting both min and max to the first element
+let min = max = nums[0];
+
+for(item of nums)
+{
+    if(item < min)
+    {
+        min = item; 
+    }
+    else if(item >max)
+    {
+        max = item; 
+    }
+}
+
+return (min+max);
+
 }
 
 // Do not edit this line;
