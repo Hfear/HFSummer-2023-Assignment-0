aa04ff997b1ab5c9891cddd11dfe649a29a89156
Hfear <hannahbfearns@gmail.com>
2023-06-09 15:24:04 -0400

    uploaded n passed tests 17 union

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,20 @@
 function setUnionOfAnyAmountOfSets(...args) {
-  // Insert code here;
+
+  let combined = new Set();
+
+  for(let i = 0; i <args.length ; i++)
+  {
+//console.log(args[i]);
+//combined.add(args[i]);
+
+    for(let j of args[i])
+    {
+      combined.add(j);
+    }
+
+  }
+   
+  return (combined);
 }
 
 // Do not edit this line;
