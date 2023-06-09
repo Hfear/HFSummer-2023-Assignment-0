bd36eea809178450977978fc3e866b42035a4ae1
Hfear <hannahbfearns@gmail.com>
2023-06-09 00:58:22 -0400

    tested and uploaded 06

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,5 +1,23 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
-  // Insert code here;
+ 
+  let count = 0; 
+
+    /**
+     * for of , cant go thru objects properties, iterates thru arrays and strings
+     * for in works with objects w properties
+     */
+
+    //item is "current index"
+    for(item of nums)
+    {
+        if(item < target)
+        {
+            count++
+        }
+    }
+
+    return (count);
+    
 }
 
 // Do not edit this line;
