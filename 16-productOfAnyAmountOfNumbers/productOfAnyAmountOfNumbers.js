b27b7431b2f320728a50ebcf0d3d5e99885ca34f
Hfear <hannahbfearns@gmail.com>
2023-06-09 11:28:55 -0400

    uploaded n passed tests 16

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,22 @@
 function productOfAnyAmountOfNumbers(...args) {
-  // Insert code here;
+  
+  let product;
+
+  for(let i=0; i< arguments.length; i++)
+  {
+      if(i == 0)
+      {
+          product = arguments[i];
+      }
+      else
+      {
+      product  *= arguments[i]; 
+      }
+      
+  }
+
+return (product);
+
 }
 
 // Do not edit this line;
