523d5a5b2ef64a1ea6f49a4adb839bc67c4f7885
Hfear <hannahbfearns@gmail.com>
2023-06-09 10:57:54 -0400

    upload n test 09

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,5 +1,18 @@
 function sumOfAllOddNumbers(nums) {
-  // Insert code here;
+  
+  let count = 0; 
+
+  for(item of nums)
+  {
+      if(item% 2 != 0)
+      {
+          count++; 
+      }
+      
+  }
+
+  return (count);
+
 }
 
 // Do not edit this line;
